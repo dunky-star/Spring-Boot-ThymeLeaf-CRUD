50b71959a752efe1ecf97b3c4eecb0f180b21f66
dunky-star <dukygeoffrey39@gmail.com>
2022-12-12 19:05:40 +0300

    Spring Boot - ThymeLeaf - CRUD database project

---
+++ spring-crud-thymeleaf/src/main/java/springcrudthymeleaf/service/EmployeeService.java
@@ -0,0 +1,15 @@
+package springcrudthymeleaf.service;
+
+import springcrudthymeleaf.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    public List<Employee> findAll();
+
+    public Employee findById(int theId);
+
+    public void save(Employee theEmployee);
+
+    public void deleteById(int theId);
+}
