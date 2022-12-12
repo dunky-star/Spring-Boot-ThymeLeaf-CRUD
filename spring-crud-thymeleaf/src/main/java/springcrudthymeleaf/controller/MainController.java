144cab5bb4784fa8bd0a083877422fc33c02ec2f
dunky-star <dukygeoffrey39@gmail.com>
2022-12-12 11:06:00 +0300

    Spring Boot, ThymeLeaf, CRUD

---
+++ spring-crud-thymeleaf/src/main/java/springcrudthymeleaf/controller/MainController.java
@@ -0,0 +1,16 @@
+package springcrudthymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    // Create a mapping for "/hello"
+    @GetMapping("/hello")
+    public String sayHello(Model theModel){
+        theModel.addAttribute("theDate", new java.util.Date());
+        return "helloworld";
+    }
+}
